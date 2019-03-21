2f9c3c82031788914e2822da0699db766893cd1f
Twan Natter <twan180401@gmail.com>
2019-03-21 10:55:24 +0100

    Activities

---
+++ app/src/main/java/nl/rocvantwente/tnatter01/tourguideapp/FamilyActivity.java
@@ -0,0 +1,13 @@
+package nl.rocvantwente.tnatter01.tourguideapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+}
